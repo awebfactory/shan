8fa2c0832ff9064c3f325e9d8c1679f4a1c6a153
victorkane <victorkane@gmail.com>
2015-08-22 16:48:55 +0000

    jwt2 installed and configured, auth set to false in every route handler

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var Hapi = require('hapi');
 var Inert = require('inert');
 var Path = require('path');
 var Sqlite3 = require('sqlite3');
+var Jwt = require('hapi-auth-jwt2');
 
 var db = new Sqlite3.Database('data/recipes.sqlite');
 
@@ -20,6 +21,38 @@ server.connection({ port: 3000 });
 server.bind({ db: db });
 
 server.register(Inert, function () {});
+
+var people = { // our "users database"
+    1: {
+      id: 1,
+      name: 'Jen Jones'
+    }
+};
+
+// bring your own validation function
+var validate = function (decoded, request, callback) {
+
+    // do your checks to see if the person is valid
+    if (!people[decoded.id]) {
+      return callback(null, false);
+    }
+    else {
+      return callback(null, true);
+    }
+};
+
+server.register(Jwt, function (err) {
+
+    if(err){
+      console.log(err);
+    }
+
+    server.auth.strategy('jwt', 'jwt', true,
+    { key: 'NeverShareYourSecret',          // Never Share your secret key
+      validateFunc: validate,            // validate function defined above
+      verifyOptions: { algorithms: [ 'HS256' ] } // pick a strong algorithm
+    });
+});
 
 server.route(require('./routes'));
 
--- routes.js
+++ routes.js
@@ -4,18 +4,22 @@ var Users = require('./handlers/users');
 module.exports = [{
     method: 'GET',
     path: '/api/recipes',
+    config: { auth: false },
     handler: Recipes.find
 }, {
     method: 'GET',
     path: '/api/recipes/{id}',
+    config: { auth: false },
     handler: Recipes.findOne
 }, {
     method: 'GET',
     path: '/api/users',
+    config: { auth: false },
     handler: Users.find
 }, {
     method: 'GET',
     path: '/{param*}',
+    config: { auth: false },
     handler: {
         directory: {
             path: '.',
