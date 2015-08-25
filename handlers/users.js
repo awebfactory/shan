b502713de4c3b881cfa719e1965284ff76c3bff9
Victor Kane <victorkane@gmail.com>
2015-08-25 16:54:20 +0000

    proper salt for bcrypt

--- handlers/users.js
+++ handlers/users.js
@@ -14,34 +14,37 @@ exports.find = function (request, reply) {
 };
 
 exports.findOne = function (request, reply) {
-	
+
 };
 
 exports.create = function (request, reply) {
-	
-	var db = this.db;
-	
-    var uuid = this.uuid.v1();
-    
-    require('bcrypt').hash(request.payload.password, 10, function (err, hash) {
-    	var password = hash
-        var sql = 'INSERT INTO users (uuid, full_name, email, username, password) VALUES (?, ?, ?, ?, ?)';
-        db.run(sql,
-        [
-            uuid,
-            request.payload.full_name,
-            request.payload.email,
-            request.payload.username,
-            password
-        ],
-        function (err) {
-    
-            if (err) {
-                throw err;
-            }
 
-            reply({ status: 'ok' });
-        });
-    })
-    		
+    var db = this.db;
+    var uuid = this.uuid.v1();
+
+    var bcrypt = require('bcrypt');
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(request.payload.password, salt, function (err, hash) {
+            var password = hash
+            var sql = 'INSERT INTO users (uuid, full_name, email, username, password) VALUES (?, ?, ?, ?, ?)';
+            db.run(sql, [
+                    uuid,
+                    request.payload.full_name,
+                    request.payload.email,
+                    request.payload.username,
+                    password
+                ],
+                function (err) {
+
+                    if (err) {
+                        throw err;
+                    }
+
+                    reply({
+                        status: 'ok'
+                    });
+                });
+        })
+    });
+
 };
