56514e38f746767f35e3f12e5a96342649866d05
Victor Kane <victorkane@gmail.com>
2015-09-02 02:25:27 +0000

    Implemented signup and cleaned up logout

--- src/ngapp/shared/users.service.js
+++ src/ngapp/shared/users.service.js
@@ -1,25 +1,36 @@
 angular.module('shared.users.service', [
 
-])
+    ])
     .factory('UserService', ['$http', '$window', function ($http, $window) {
         var currentUser = {};
         var setCurrentUser = function (res) {
-                var store = $window.localStorage;
-                var key = 'auth-token';
-                currentUser = res.data.user;
+            var store = $window.localStorage;
+            var key = 'auth-token';
+            currentUser = res.data.user;
 
-                if (res.data.token) {
-                    store.setItem(key, res.data.token);
-                    console.log('currentUser signed in user is: ', currentUser);
-                } else {
-                    store.removeItem(key);
-                    console.log('currentUser signed out');
-                }
+            if (res.data.token) {
+                store.setItem(key, res.data.token);
+                console.log('currentUser signed in user is: ', currentUser);
+            } else {
+                store.removeItem(key);
+                console.log('currentUser signed out');
             }
+        }
+        //TODO make reusable filter http://www.java2s.com/Tutorials/AngularJS/AngularJS_Example/Filter/Filter_to_remove_space_and_lowercase_with_regular_expression.htm
+        var removeSpacesThenLowercase = function (text) {
+            console.log('before: ', text);
+            var str = text.replace(/\s+/g, '');
+            console.log('after: ', str);
+            return str.toLowerCase();
+        }
         return {
             addUser: function (user) {
-                $http.post('/api/users', user)
-                    .then(grabUsers);
+                user.username = removeSpacesThenLowercase(user.full_name);
+                console.log(user);
+                return $http.post('/api/users', user)
+                    .then(function success(res) {
+                        return res;
+                    });
             },
             login: function (user) {
                 return $http.post('/login', user)
@@ -29,7 +40,9 @@ angular.module('shared.users.service', [
                     });
             },
             logout: function () {
-                var nullres = {'data': {}};
+                var nullres = {
+                    'data': {}
+                };
                 nullres.data.user = {};
                 nullres.data.token = '';
                 setCurrentUser(nullres);
--- src/ngapp/users/users.module.js
+++ src/ngapp/users/users.module.js
@@ -10,29 +10,46 @@ angular.module('users', [
         usersCtrl.emailFormat = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
         usersCtrl.user = {};
         usersCtrl.newuser = {};
+        usersCtrl.currentUser = {};
         usersCtrl.signin = function () {
             UserService.login(usersCtrl.user).then(
                 function success(res) {
+                    // reset form and model
+                    usersCtrl.user = {};
                     usersCtrl.tab = 'off';
-                    usersCtrl.user = res.data.user;
-                    console.log('User signed in successfully: ', usersCtrl.user);
+                    usersCtrl.currentUser = res.data.user;
+                    //console.log('User signed in successfully: ', usersCtrl.currentUser);
                 },
                 function error (res) {
                     usersCtrl.tab = 'signup';
-                    console.log('Error on signup for', usersCtrl.user);
-                    console.log(res);
+                    //console.log('Error on signup for', usersCtrl.user);
+                    //console.log(res);
                     alert('Error: ' + res.data);
                 }
             );
         };
         usersCtrl.signup = function () {
-            if (true) {
-                UserService.addUser(usersCtrl.newuser);
-                console.log('User signed up: ', usersCtrl.newuser)
+            UserService.addUser(usersCtrl.newuser).then(
+                function success(res) {
                     // reset form and model
-                usersCtrl.newuser = {};
+                    usersCtrl.newuser = {};
+                    usersCtrl.tab = 'signin';
+                    //console.log('User signed up: ', usersCtrl.newuser)
+                }
+            );
+        };
+        usersCtrl.currentUserName = function () {
+            usersCtrl.currentUser = UserService.getCurrentUser();
+            //console.log('user: ', usersCtrl.currentUser);
+            if (_.isEmpty(usersCtrl.currentUser)) {
+                if (usersCtrl.tab == 'off') {
+                    usersCtrl.tab = 'signin';
+                }
+                console.log('no user logged in')
+                return '';
             } else {
-                console.log('Registered user attempted to sign up', usersCtrl.newuser)
+                console.log('user is logged in')
+                return usersCtrl.currentUser.email;
             }
         };
     }]);
