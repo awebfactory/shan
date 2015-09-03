c338b13d5b5e8539a751b506dcaf34858b3796a3
Victor Kane <victorkane@gmail.com>
2015-09-03 15:56:37 +0000

    Sub view is working with parameterized sref and successfully invoking the sub view template. No transmission of data yet or associated controller

--- src/ngapp/recipes/recipes.module.js
+++ src/ngapp/recipes/recipes.module.js
@@ -1,6 +1,18 @@
 angular.module('recipes', [
     'shared.recipes.service'
 ])
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('recipejs.view-recipe', {
+        	url:  '/recipes/:slug',
+        	views: {
+        		'recipes@': {
+        	        templateUrl: "recipes/recipes.view-recipe.tmpl.html"
+        	        //controller: "RecipesCtrl"
+        		}
+        	}
+        });
+})
 .controller('RecipesCtrl', ['RecipeService', function (RecipeService) {
     var recipesCtrl = this;
     recipesCtrl.list = function() {
