285bbaed3879fa7a675bc2ba478fffbc4a8dd02d
Victor Kane <victorkane@gmail.com>
2015-09-04 15:33:30 +0000

    associated controller specified for recipejs view-recipe sub view

--- src/ngapp/recipes/recipes.module.js
+++ src/ngapp/recipes/recipes.module.js
@@ -7,16 +7,22 @@ angular.module('recipes', [
         	url:  '/recipes/:slug',
         	views: {
         		'recipes@': {
-        	        templateUrl: "recipes/recipes.view-recipe.tmpl.html"
-        	        //controller: "RecipesCtrl"
+        	        templateUrl: "recipes/recipes.view-recipe.tmpl.html",
+        	 //       controller: function($scope, $stateParams) {
+        	 //       	console.log('stateParams: ', $stateParams);
+        	        controller: 'RecipesCtrl as recipesCtrl'
         		}
         	}
         });
 })
-.controller('RecipesCtrl', ['RecipeService', function (RecipeService) {
+.controller('RecipesCtrl', ['RecipeService', '$stateParams', function (RecipeService, $stateParams) {
     var recipesCtrl = this;
     recipesCtrl.list = function() {
         return RecipeService.listRecipes();
+    };
+    recipesCtrl.recipeBySlug = function() {
+        //console.log('the stateParams: ', $stateParams);
+    	return $stateParams.slug;
     };
 }])
 .directive('recipe', [function() {
